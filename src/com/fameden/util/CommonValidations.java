bd22440570cc87fc167f5df5a14cfc67fd123b83
ravjot28 <ravjot28@gmail.com>
2013-08-12 13:23:23 +0530

    Renamed the interface reference

--- src/com/fameden/util/CommonValidations.java
+++ src/com/fameden/util/CommonValidations.java
@@ -4,6 +4,9 @@
  */
 package com.fameden.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Ravjot
@@ -15,6 +18,22 @@ public class CommonValidations {
         if (data == null || data.trim().length() == 0) {
             result = true;
         }
+        return result;
+    }
+
+    public static boolean isValidEmailAddress(String emailAddress) {
+        boolean result = false;
+
+        if (!isStringEmpty(emailAddress)) {
+            String EMAIL_PATTERN =
+                    "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+            Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+            Matcher matcher = pattern.matcher(emailAddress);
+            result =  matcher.matches();
+        }
+
         return result;
     }
 }
