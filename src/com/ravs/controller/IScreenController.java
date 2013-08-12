a759d767887dd964691d61b6ef23fe7182bc453e
ravjot28 <ravjot28@gmail.com>
2013-08-12 12:24:46 +0530

    Renamed the file to meet the standards

---
+++ src/com/ravs/controller/IScreenController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ravs.controller;
+
+import com.ravs.fxml.ScreenController;
+
+/**
+ *
+ * @author Ravjot
+ */
+public interface IScreenController {
+    
+    public void setScreenParent(ScreenController screenPage);
+    
+}
